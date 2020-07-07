e2b68b26b85381b5c5fa7cd1f592b8a7a527fe3b
Peter Schmidt <schmidt.multimedia@gmail.com>
2020-07-07 09:46:07 +0200

    Files uploaded

---
+++ challenge_01.js
@@ -0,0 +1,10 @@
+
+/*********   CODE CHALLENGE 1 **********/
+ 
+/*
+Geben Sie über eine Promt-Anforderung eine Zahl ein
+Geben Sie über eine Promt-Anforderung eine zweite Zahl ein
+Berechnen Sie die Summe der Zahlen.
+Geben Sie die Summe der Zahlen in die Konsole aus:
+‘Die Summe der Zahlen ist: summe ‘
+*/
